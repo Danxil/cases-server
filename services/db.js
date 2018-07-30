d5b6091eb619037acb6869ddd29174494b602754
Vadym Bondarenko <danxil@list.ru>
2018-07-30 19:42:04 +0300

    in progress

--- services/db.js
+++ services/db.js
@@ -2,18 +2,18 @@ import Sequelize from 'sequelize';
 import models from '../models';
 
 export default () => {
-  const options = {
-    sync: { force: process.env.NODE_ENV !== 'production' },
-  };
   const dbConnection = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      ...options,
+      sync: { force: process.env.NODE_ENV !== 'production' },
       host: process.env.DB_HOST,
       dialect: 'postgres',
       logging: false,
+      dialectOptions: {
+        ssl: true,
+      },
     },
   );
 
