875cf45ad3ffe88863a0d21f90144b7549d89a35
Vadym Bondarenko <danxil@list.ru>
2018-08-03 20:57:11 +0300

    Destroy after expired

--- gameConfig.js
+++ gameConfig.js
@@ -1,4 +1,4 @@
-export const GAME_MIN_ALIVE_GAMES_AMOUNT = 42;
+export const GAME_MIN_ALIVE_GAMES_AMOUNT = 200;
 export const GAME_GAME_TIMEOUT = 30000;
 export const GAME_USER_TIMEOUT = 15000;
 export const GAME_GAME_SPIN_DELAY = 12000;
@@ -6,8 +6,8 @@ export const GAME_CHECK_ALIVE_GAMES_INTERVAL = 2000;
 export const GAME_MIN_CHANCE_TO_WIN = 5;
 export const GAME_MAX_CHANCE_TO_WIN = 95;
 export const GAME_MAX_ATTEMPTS = 20;
-export const GAME_MIN_PRIZE = 5;
-export const GAME_MAX_PRIZE = 100;
+export const GAME_MIN_PRIZE = 1;
+export const GAME_MAX_PRIZE = 200;
 export const GAME_ADD_BOT_INTERVAL = 5000;
 export const GAME_UPDATE_BOTS_PHOTOS = 1800000;
 export const GAME_UPDATE_STATISTIC_BOTS = 43200000;
