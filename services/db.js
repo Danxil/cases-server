f58e5c7045a8b3662f07014fbc361f5c8e18b88b
Vadym Bondarenko <danxil@list.ru>
2018-08-16 01:50:16 +0300

    Changes

--- services/db.js
+++ services/db.js
@@ -8,8 +8,8 @@ export default async () => {
   const dbConnection = new Sequelize(
     process.env.DATABASE_URL,
     {
-      sync: { force: process.env.NODE_ENV !== 'production' },
-      // sync: { force: false },
+      // sync: { force: process.env.NODE_ENV !== 'production' },
+      sync: { force: false },
       logging: str => debug(str),
       pool: {
         max: 5,
