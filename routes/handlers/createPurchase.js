03d284cbf92e8a8cd50bf788d6b950aeba589478
Vadym Bondarenko <danxil@list.ru>
2018-08-30 23:05:14 +0300

    Added kassa handlers

--- routes/handlers/createPurchase.js
+++ routes/handlers/createPurchase.js
@@ -1,8 +1,9 @@
 import { createPurchase } from '../../controllers/purchases';
 
 export default () => async (req) => {
-  const { AMOUNT: amount } = req.query;
-  const user = await global.db.User.findOne({ where: { id: req.query.us_userId } })
+  console.log(11, req.query);
+  const { AMOUNT: amount, us_userId } = req.query;
+  const user = await global.db.User.findOne({ where: { id: us_userId } })
   const { balance, id: userId } = user;
   const updatedUser = await user.update({ balance: balance + parseFloat(amount) });
   await createPurchase({
