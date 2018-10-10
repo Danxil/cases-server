b6246382a481918c19b363b8381363a31db000ec
Vadym Bondarenko <danxil@list.ru>
2018-10-10 15:40:50 +0300

    Fixed promise

--- configs/configureSchedules.js
+++ configs/configureSchedules.js
@@ -43,4 +43,17 @@ export default async () => {
       console.log(e);
     }
   }, GAME_UPDATE_FAKES_INTERVAL);
+
+  setInterval(() => {
+    const heapUsed = process.memoryUsage().heapUsed / 1024 / 1024;
+    const heapTotal = process.memoryUsage().heapTotal / 1024 / 1024;
+    const rss = process.memoryUsage().rss / 1024 / 1024;
+    const external = process.memoryUsage().external / 1024 / 1024;
+    console.log('======');
+    console.log(`The script uses approximately heapUsed ${Math.round(heapUsed * 100) / 100} MB`);
+    console.log(`The script uses approximately heapTotal ${Math.round(heapTotal * 100) / 100} MB`);
+    console.log(`The script uses approximately rss ${Math.round(rss * 100) / 100} MB`);
+    console.log(`The script uses approximately external ${Math.round(external * 100) / 100} MB`);
+    console.log('======');
+  }, 1000);
 };
